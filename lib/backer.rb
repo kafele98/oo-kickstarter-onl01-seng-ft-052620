d67cc667e381133e6f4749299bea65e86f8b9c6e
Zoe Keller <keller.zoe98@gmail.com>
2020-06-16 12:32:04 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,10 @@
 class Backer 
   attr_reader :name 
   
-  def initialization(name, backed_projects)
+  def initialize(name, backed_projects)
     @name = name 
     @backed_projects = []
   end 
   
 end 
-
-
-backer = Backer.new("Avi")
+ 
